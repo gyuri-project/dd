eba3b3899c7a2df6d024fc21c32231c87c80c69d
gyuri-project <92837624+gyuri-project@users.noreply.github.com>
2024-06-26 17:41:56 +0900

    Add files via upload

--- script/sub1.js
+++ script/sub1.js
@@ -1,12 +1,25 @@
-// $(window).scroll(function () {
-//     var height = $(window).scrollTop();
-//     if (height > 4700) {
-//         $('.logo').addClass('on');
-//     } else {
-//         $('.logo').removeClass('on');
-//     }
+wow = new WOW(
+    {
+        animateClass: 'animated',
+        offset: 100,
+        callback: function (box) {
+            console.log("WOW: animating <" + box.tagName.toLowerCase() + ">")
+        }
+    }
+);
+wow.init();
+
+
+$(window).scroll(function () {
+    var height = $(window).scrollTop();
+    if (height > 4700) {
+        $('.logo').addClass('on');
+    } else {
+        $('.logo').removeClass('on');
+    }
+});
+
+// AOS.init({
+//     duration: 1200 //aos 나타나는 속도
 // });
 
-AOS.init({
-    duration: 1200 //aos 나타나는 속도
-});
